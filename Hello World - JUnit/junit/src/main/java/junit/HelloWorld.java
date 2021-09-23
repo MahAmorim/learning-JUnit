32964085942105f226be662e2aff578935131399
MahAmorim <61316782+MahAmorim@users.noreply.github.com>
2021-09-23 18:36:22 -0300

    :test_tube: Add Test Hello World

---
+++ Hello World - JUnit/junit/src/main/java/junit/HelloWorld.java
@@ -0,0 +1,15 @@
+package junit;
+
+public final class HelloWorld {
+    private HelloWorld() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Hello World - JUnit/junit/src/test/java/junit/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldTest {
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
